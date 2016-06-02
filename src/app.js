31b9a8dde49f764b30ba37a4876fe10158a3cce4
satish.verma143 <satish.verma143@gmail.com>
2016-06-02 16:02:51 +0530

    step 2 final done with basic design

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import Angular from 'angular';
+import Ionic from 'ionic';
+import Keyboard from 'cordova/keyboard';
+import StatusBar from 'cordova/status-bar';
+import Loader from 'angular-ecmascript/module-loader';
+
+import ChatsCtrl from './controllers/chats.controller';
+import CalendarFilter from './filters/calendar.filter';
+import RoutesConfig from './routes';
+
+const App = 'whatsapp';
+
+Angular.module(App, [
+  'ionic'
+]);
+
+new Loader(App)
+  .load(ChatsCtrl)
+  .load(CalendarFilter)
+  .load(RoutesConfig);
+
+Ionic.Platform.ready(() => {
+  if (Keyboard) {
+    Keyboard.hideKeyboardAccessoryBar(true);
+    Keyboard.disableScroll(true);
+  }
+
+  if (StatusBar) {
+    StatusBar.styleLightContent();
+  }
+
+  Angular.bootstrap(document, [App]);
+});
